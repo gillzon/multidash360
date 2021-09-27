5209c3a75098d0fd242929d4eb1263b2d810de2d
gillzon <oscargillberg@gmail.com>
2021-09-27 12:05:50 +0200

    alot

--- server/index.js
+++ server/index.js
@@ -34,39 +34,40 @@ function generateUrl(ips) {
 app.get("/v1/get_all_xbox", (req, res) => {
   let xboxips = req.query.xbox
   let xboxdata = []
-  let promises = generateUrl(xboxips)
-  console.log("prmises", promises)
-  xboxdata = promises[1]
-  Promise.allSettled(promises[0].map(function (entity) {
-    return Promise.allSettled(entity.data.map(function (item) {
-      return item;
-    }));
-  })).then(function (data) {
-    for (var i = 0, l = data.length; i < l; i++) {
-      for (k in data[i].value) {
-        if (data[i].value[k].status == "rejected") {
-          var errorurl = new URL(data[i].value[k].reason.config.url)
-          let formated_url = errorurl.pathname.replace("/", "")
-          if (formated_url === "title/live/cache") {
-            formated_url = "live"
+  if (xboxips !== undefined) {
+    let promises = generateUrl(xboxips)
+    console.log("prmises", promises)
+    xboxdata = promises[1]
+    Promise.allSettled(promises[0].map(function (entity) {
+      return Promise.allSettled(entity.data.map(function (item) {
+        return item;
+      }));
+    })).then(function (data) {
+      for (var i = 0, l = data.length; i < l; i++) {
+        for (k in data[i].value) {
+          if (data[i].value[k].status == "rejected") {
+            var errorurl = new URL(data[i].value[k].reason.config.url)
+            let formated_url = errorurl.pathname.replace("/", "")
+            if (formated_url === "title/live/cache") {
+              formated_url = "live"
+            }
+            var foundIndex = xboxdata.findIndex(x => x.xbox == errorurl.hostname);
+            xboxdata[foundIndex].data[formated_url] = { 'error': 'not able to reach xbox' }
           }
-          var foundIndex = xboxdata.findIndex(x => x.xbox == errorurl.hostname);
-          xboxdata[foundIndex].data[formated_url] = { 'error': 'not able to reach xbox' }
-        }
-        if (data[i].value[k].status == "fulfilled") {
-          var url = new URL(data[i].value[k].value.config.url)
-          var foundIndex = xboxdata.findIndex(x => x.xbox == url.hostname);
-          let formated_url = url.pathname.replace("/", "")
-          if (formated_url === "title/live/cache") {
-            formated_url = "live"
+          if (data[i].value[k].status == "fulfilled") {
+            var url = new URL(data[i].value[k].value.config.url)
+            var foundIndex = xboxdata.findIndex(x => x.xbox == url.hostname);
+            let formated_url = url.pathname.replace("/", "")
+            if (formated_url === "title/live/cache") {
+              formated_url = "live"
+            }
+            xboxdata[foundIndex].data[formated_url] = data[i].value[k].value.data
           }
-          xboxdata[foundIndex].data[formated_url] = data[i].value[k].value.data
         }
       }
-    }
-    res.json(xboxdata)
-  })
-
+      res.json(xboxdata)
+    })
+  }
 });
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
